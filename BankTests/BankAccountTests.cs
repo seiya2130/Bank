6da6c682451d096a83cd510b94791a60e037099f
Seiya_Satoh <satohseiya0312@gmail.com>
2021-08-10 08:52:53 +0900

    [update]エラーの詳細化

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -40,11 +40,22 @@ namespace BankTests
         {
             // Arrange
             double beginningBalance = 11.99;
-            double debitAmount = 12.00;
+            double debitAmount = 20.0;
             BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
 
-            // Act and assert
-            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+            // Act
+            try
+            {
+                account.Debit(debitAmount);
+            }
+            catch (System.ArgumentOutOfRangeException e)
+            {
+                // Assert
+                StringAssert.Contains(e.Message, BankAccount.DebitAmountExceedsBalanceMessage);
+                return;
+            }
+
+            Assert.Fail("The expected exception was not thrown.");
         }
 
         //ï™êÕÇåpë±Ç∑ÇÈ
